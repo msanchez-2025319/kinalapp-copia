78de2c0a2a957b38b028b6e5c01acf976f0c8d1e
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-26 01:08:42 -0600

    feat: se implemento es método findByEstado en UsuarioRepository

--- src/main/java/com/mynorsanchez/kinalapp/repository/UsuarioRepository.java
+++ src/main/java/com/mynorsanchez/kinalapp/repository/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package com.mynorsanchez.kinalapp.repository;
 
+import com.mynorsanchez.kinalapp.entity.Cliente;
 import com.mynorsanchez.kinalapp.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface UsuarioRepository extends JpaRepository <Usuario,Long> {
+    List<Usuario> findByEstado(Long estado);
 }
