b092f9ba1030ee1bbcb67382e617f66487114cff
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-25 23:25:24 -0600

    feat: Se añadió el método buscarPorCodigoU en IUsuarioService

--- src/main/java/com/mynorsanchez/kinalapp/service/IUsuarioService.java
+++ src/main/java/com/mynorsanchez/kinalapp/service/IUsuarioService.java
@@ -3,10 +3,14 @@ package com.mynorsanchez.kinalapp.service;
 import com.mynorsanchez.kinalapp.entity.Usuario;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IUsuarioService {
 List<Usuario> listarTodo();
 List<Usuario> listarActivos();
 
 Usuario guardar (Usuario usuario);
+
+Optional<Usuario> buscarPorCodigoU(String codigoU);
+
 }
