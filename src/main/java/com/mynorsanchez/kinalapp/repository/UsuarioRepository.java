d5f3593879f078ab7801b6aa7ff9facb3047e45d
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-25 22:20:27 -0600

    feat: se agrego UsuarioRepository

---
+++ src/main/java/com/mynorsanchez/kinalapp/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.mynorsanchez.kinalapp.repository;
+
+import com.mynorsanchez.kinalapp.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UsuarioRepository extends JpaRepository <Usuario,String> {
+}
