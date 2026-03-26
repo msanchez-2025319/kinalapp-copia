ecb93f423fee2355415b814082ea611559aaa713
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-26 11:15:17 -0600

    feat: se agrego el ProductoRepository

--- src/main/java/com/mynorsanchez/kinalapp/repository/ProductoRepository.java
+++ src/main/java/com/mynorsanchez/kinalapp/repository/ProductoRepository.java
@@ -1,4 +1,10 @@
 package com.mynorsanchez.kinalapp.repository;
 
-public interface ProductoRepository {
+import com.mynorsanchez.kinalapp.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductoRepository extends JpaRepository <Producto, String> {
+    List<Producto> finByEstado(String producto);
 }
