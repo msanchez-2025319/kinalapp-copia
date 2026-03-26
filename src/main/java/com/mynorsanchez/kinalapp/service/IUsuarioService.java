7b85d901fcc8e8f23698ae28bf5a6df8209090cc
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-25 22:56:02 -0600

    feat: se añadieron los métodos listarTodo y listarActivos en IUsuarioService

--- src/main/java/com/mynorsanchez/kinalapp/service/IUsuarioService.java
+++ src/main/java/com/mynorsanchez/kinalapp/service/IUsuarioService.java
@@ -1,4 +1,11 @@
 package com.mynorsanchez.kinalapp.service;
 
+import com.mynorsanchez.kinalapp.entity.Usuario;
+
+import java.util.List;
+
 public interface IUsuarioService {
+List<Usuario> listarTodo();
+List<Usuario> listarActivos();
+
 }
