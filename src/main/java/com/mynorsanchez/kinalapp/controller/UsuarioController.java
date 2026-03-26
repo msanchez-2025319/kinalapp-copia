18922b03083b10fd73cbb754b6a23917e57858eb
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-26 02:30:29 -0600

    feat: se añadió el método listar en UsuarioController

--- src/main/java/com/mynorsanchez/kinalapp/controller/UsuarioController.java
+++ src/main/java/com/mynorsanchez/kinalapp/controller/UsuarioController.java
@@ -1,4 +1,28 @@
 package com.mynorsanchez.kinalapp.controller;
 
+import com.mynorsanchez.kinalapp.entity.Usuario;
+import com.mynorsanchez.kinalapp.service.IUsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Usorios")
+
 public class UsuarioController {
+
+    private final IUsuarioService usuarioService;
+
+    public UsuarioController(IUsuarioService usuarioService) { this.usuarioService = usuarioService; }
+
+    @GetMapping
+
+    public ResponseEntity<List<Usuario>> listar(){
+        List<Usuario> usuarios = usuarioService.listarTodo();
+        return ResponseEntity.ok(usuarios);
+    }
+
 }
