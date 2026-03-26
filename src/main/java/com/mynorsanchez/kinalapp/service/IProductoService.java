ba31581ced6b7dd4f9422c1c41e58c8bcd5f4791
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-26 17:19:11 -0600

    feat: se añadio el metodo buscarPorCodigoProducto en IProductoService

--- src/main/java/com/mynorsanchez/kinalapp/service/IProductoService.java
+++ src/main/java/com/mynorsanchez/kinalapp/service/IProductoService.java
@@ -3,10 +3,13 @@ package com.mynorsanchez.kinalapp.service;
 import com.mynorsanchez.kinalapp.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IProductoService {
     List<Producto> listarTodo();
     List<Producto> listarActivos();
 
     Producto guardar (Producto producto);
+
+    Optional<Producto> buscarPorCodigoProducto(String codigoProducto);
 }
