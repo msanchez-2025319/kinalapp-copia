33583801b6ea642dfe8fe79f3c1226a0fc390fca
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-26 01:05:04 -0600

    refactor: se cambia el tipo de parámetro de String a Long en UsuarioRepository

--- src/main/java/com/mynorsanchez/kinalapp/repository/UsuarioRepository.java
+++ src/main/java/com/mynorsanchez/kinalapp/repository/UsuarioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface UsuarioRepository extends JpaRepository <Usuario,String> {
+public interface UsuarioRepository extends JpaRepository <Usuario,Long> {
 }
--- src/main/java/com/mynorsanchez/kinalapp/service/UsuarioService.java
+++ src/main/java/com/mynorsanchez/kinalapp/service/UsuarioService.java
@@ -1,4 +1,54 @@
 package com.mynorsanchez.kinalapp.service;
 
-public class UsuarioService {
+import com.mynorsanchez.kinalapp.entity.Usuario;
+import com.mynorsanchez.kinalapp.repository.UsuarioRepository;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+
+public class UsuarioService implements IUsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Usuario> listarTodo() { return usuarioRepository.findAll();}
+
+    @Override
+    public List<Usuario> listarActivos() { return usuarioRepository.findByEstado(1L);}
+
+    @Override
+    public Usuario guardar(Usuario usuario) {
+        return null;
+    }
+
+    @Override
+    public Optional<Usuario> buscarPorCodigoU(Long codigoU) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Usuario actualizar(Long codigoU, Usuario usuario) {
+        return null;
+    }
+
+    @Override
+    public void eliminar(Long codigoU) {
+
+    }
+
+    @Override
+    public boolean existePorCodigoU(Long codigoU) {
+        return false;
+    }
 }
