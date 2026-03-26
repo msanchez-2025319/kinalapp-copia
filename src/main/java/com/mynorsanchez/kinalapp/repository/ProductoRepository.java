a8d30c147f2410cfcd8922a0c47b614937112987
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-26 17:27:53 -0600

    fix: se cambio el tipo de atributo de string a Long em Producto, ProductoRepository y IProductoService

--- src/main/java/com/mynorsanchez/kinalapp/repository/ProductoRepository.java
+++ src/main/java/com/mynorsanchez/kinalapp/repository/ProductoRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ProductoRepository extends JpaRepository <Producto, String> {
-    List<Producto> finByEstado(String producto);
+public interface ProductoRepository extends JpaRepository <Producto, Long> {
+    List<Producto> finByEstado(Long estado);
 }
