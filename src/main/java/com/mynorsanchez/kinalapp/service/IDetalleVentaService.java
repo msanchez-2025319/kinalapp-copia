bbff7b78a21c5ea80171c85da4cb55e73d5c53ce
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-27 11:25:39 -0600

    feat: agregaron los métodos listarTodo y listarActivos en IDetalleVentaService

--- src/main/java/com/mynorsanchez/kinalapp/service/IDetalleVentaService.java
+++ src/main/java/com/mynorsanchez/kinalapp/service/IDetalleVentaService.java
@@ -1,4 +1,12 @@
 package com.mynorsanchez.kinalapp.service;
 
+import com.mynorsanchez.kinalapp.entity.Producto;
+
+import java.util.List;
+
 public interface IDetalleVentaService {
+
+    List<Producto> listarTodo();
+    List<Producto> listarActivos();
+
 }
