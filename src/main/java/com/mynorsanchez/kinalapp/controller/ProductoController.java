8ff73757c982bd606228fea1591241e55d23f8b0
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-26 22:31:47 -0600

    feat: se añadio el endpoint buscarPorCodigoProducto en ProductoController

--- src/main/java/com/mynorsanchez/kinalapp/controller/ProductoController.java
+++ src/main/java/com/mynorsanchez/kinalapp/controller/ProductoController.java
@@ -5,6 +5,7 @@ import com.mynorsanchez.kinalapp.entity.Producto;
 import com.mynorsanchez.kinalapp.service.IProductoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,8 @@ public class ProductoController {
         List<Producto> productos = productoService.listarTodo();
         return ResponseEntity.ok(productos);
     }
-
+    @GetMapping("/{codigoProducto}")
+    public ResponseEntity<Producto> buscarPorCodigoProducto(@PathVariable Long codigoProducto){
+        return productoService.buscarPorCodigoProducto(codigoProducto).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
 }
