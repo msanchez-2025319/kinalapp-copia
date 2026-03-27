d5a5b9300e006b1eabd330805d15ca12ec08a119
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-27 02:33:15 -0600

    feat: se creo el endpoint lista a VentaController

--- src/main/java/com/mynorsanchez/kinalapp/controller/VentaController.java
+++ src/main/java/com/mynorsanchez/kinalapp/controller/VentaController.java
@@ -1,4 +1,25 @@
 package com.mynorsanchez.kinalapp.controller;
 
+import com.mynorsanchez.kinalapp.entity.Usuario;
+import com.mynorsanchez.kinalapp.entity.Venta;
+import com.mynorsanchez.kinalapp.service.IVentaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Ventas")
 public class VentaController {
+    private final IVentaService ventaService;
+
+    public VentaController(IVentaService ventaService) { this.ventaService = ventaService; }
+
+    @GetMapping
+    public ResponseEntity<List<Venta>> listar(){
+        List<Venta> ventas = ventaService.listarTodo();
+        return ResponseEntity.ok(ventas);
+    }
 }
