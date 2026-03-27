f19e76c0ff50269499c3c6072eed2592037194f7
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-27 03:50:52 -0600

    feat: se crearon atributos y relaciones en DetalleVenta

--- src/main/java/com/mynorsanchez/kinalapp/entity/DetalleVenta.java
+++ src/main/java/com/mynorsanchez/kinalapp/entity/DetalleVenta.java
@@ -1,4 +1,31 @@
 package com.mynorsanchez.kinalapp.entity;
 
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "DetalleVenta")
 public class DetalleVenta {
+    @Id
+    @Column(name = "codigo_detalle_venta")
+   private Long codigoDetalleVenta;
+
+    @Column
+    private Long cantidad;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal precioUnitario;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal subtotal;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Producto_codigo_producto")
+    private Producto producto;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Ventas_codigo_venta")
+    private  Venta venta;
 }
