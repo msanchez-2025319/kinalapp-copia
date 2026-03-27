e9ef4e21aacdbd4a228d72c7ae30640618a48176
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-26 22:25:41 -0600

    feat: se añadio el endpoint listar en ProductoController

--- src/main/java/com/mynorsanchez/kinalapp/controller/ProductoController.java
+++ src/main/java/com/mynorsanchez/kinalapp/controller/ProductoController.java
@@ -1,4 +1,28 @@
 package com.mynorsanchez.kinalapp.controller;
 
+
+import com.mynorsanchez.kinalapp.entity.Producto;
+import com.mynorsanchez.kinalapp.service.IProductoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
 public class ProductoController {
+    private final IProductoService productoService;
+
+    public ProductoController(IProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Producto>> listar(){
+        List<Producto> productos = productoService.listarTodo();
+        return ResponseEntity.ok(productos);
+    }
+
 }
