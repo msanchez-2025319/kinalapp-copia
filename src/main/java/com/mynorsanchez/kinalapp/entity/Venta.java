3e65d7a3cd9acd7bc82a74c8f069f4e5231dcea8
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-27 00:29:44 -0600

    feat: se crearon los campos principales y las relaciones de cliente y Usuario a Venta

--- src/main/java/com/mynorsanchez/kinalapp/entity/Venta.java
+++ src/main/java/com/mynorsanchez/kinalapp/entity/Venta.java
@@ -1,4 +1,35 @@
 package com.mynorsanchez.kinalapp.entity;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Venta")
 public class Venta {
+    @Id
+    @Column (name = "codigo_venta")
+    private Long codigoVenta;
+
+    @Column
+    private LocalDate fechaVenta;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal total;
+
+    @Column
+    private Long estado;
+
+    /*las llaves foráneas no llevan long*/
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Clientes_dpi_cliente")
+    private Cliente cliente;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Usuario_codigo_usuario")
+    private Usuario usuario;
+
+    
+
 }
