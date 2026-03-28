8289ceeb5d0a0a77232b5caf6d953155db1fcb89
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-03-27 18:13:23 -0600

    feat: se implementaron buscarPorCodigoDetalleVenta DetalleVentaController

--- src/main/java/com/mynorsanchez/kinalapp/controller/DetalleVentaController.java
+++ src/main/java/com/mynorsanchez/kinalapp/controller/DetalleVentaController.java
@@ -1,4 +1,23 @@
 package com.mynorsanchez.kinalapp.controller;
 
+import com.mynorsanchez.kinalapp.entity.DetalleVenta;
+import com.mynorsanchez.kinalapp.service.IDetalleVentaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/detalleVenta")
 public class DetalleVentaController {
+    private final IDetalleVentaService detalleVentaService;
+
+    public DetalleVentaController(IDetalleVentaService detalleVentaService) {
+        this.detalleVentaService = detalleVentaService;
+    }
+
+    @GetMapping("/{codigoDetalleVenta}")
+    public ResponseEntity<DetalleVenta> buscarPorCodigoDetalleVenta(@PathVariable Long codigoDetalleVenta){
+        return detalleVentaService.buscarPorCodigoDetalleVenta(codigoDetalleVenta).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+   
 }
