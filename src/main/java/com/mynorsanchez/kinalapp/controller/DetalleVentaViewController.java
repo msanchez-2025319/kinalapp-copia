d81c46d6233bb8b81b53d7b4ea990f6ed0cbaba7
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-04-15 07:22:45 -0600

    fix: correcciones en DetalleVentaViewController

--- src/main/java/com/mynorsanchez/kinalapp/controller/DetalleVentaViewController.java
+++ src/main/java/com/mynorsanchez/kinalapp/controller/DetalleVentaViewController.java
@@ -1,14 +1,11 @@
-package com.mynorsanchez.kinalapp.controller;
+package com.mynorsanchez.kinalapp.view;
 
 import com.mynorsanchez.kinalapp.entity.DetalleVenta;
 import com.mynorsanchez.kinalapp.service.IDetalleVentaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/vista/detalle-venta")
@@ -18,19 +15,19 @@ public class DetalleVentaViewController {
     private IDetalleVentaService detalleVentaService;
 
     @GetMapping
-    public String listar(Model model){
-        model.addAttribute("detalle", detalleVentaService.listarTodo());
+    public String listar(Model model) {
+        model.addAttribute("detalles", detalleVentaService.listarTodo());
         return "detalle-venta/lista";
     }
 
     @GetMapping("/nuevo")
-    public String formularioNuevo(Model model){
+    public String formularioNuevo(Model model) {
         model.addAttribute("detalle", new DetalleVenta());
         return "detalle-venta/formulario";
     }
 
     @PostMapping("/guardar")
-    public String guardar(@ModelAttribute DetalleVenta detalle){
+    public String guardar(@ModelAttribute DetalleVenta detalle) {
         detalleVentaService.guardar(detalle);
         return "redirect:/vista/detalle-venta";
     }
