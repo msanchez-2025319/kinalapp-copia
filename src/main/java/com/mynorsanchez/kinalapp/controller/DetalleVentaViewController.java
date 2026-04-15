d654d91a3b88cdba3dd0b732910c467bdc983cb2
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-04-15 03:01:39 -0600

    feat: se agrego en DetalleVentaViewController el endpoint de listar

--- src/main/java/com/mynorsanchez/kinalapp/controller/DetalleVentaViewController.java
+++ src/main/java/com/mynorsanchez/kinalapp/controller/DetalleVentaViewController.java
@@ -1,4 +1,22 @@
 package com.mynorsanchez.kinalapp.controller;
 
+import com.mynorsanchez.kinalapp.service.IDetalleVentaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vista/detalle-venta")
 public class DetalleVentaViewController {
+
+    @Autowired
+    private IDetalleVentaService detalleVentaService;
+
+    @GetMapping
+    public String listar(Model model){
+        model.addAttribute("detalle", detalleVentaService.listarTodo());
+        return "detalle-venta/lista";
+    }
 }
