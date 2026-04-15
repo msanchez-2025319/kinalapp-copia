839e95cba291e957433fab0671d050c5d1c25372
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-04-15 02:14:38 -0600

    feat: se agrego en UsuarioViewController el endpoint de guardar

--- src/main/java/com/mynorsanchez/kinalapp/controller/UsuarioViewController.java
+++ src/main/java/com/mynorsanchez/kinalapp/controller/UsuarioViewController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,5 +25,10 @@ public class UsuarioViewController {
     public String formularioNuevo(Model model){
         model.addAttribute("usuario", new Usuario());
         return "usuario/formulario";
+    }
+    @PostMapping("/guardar")
+    public String guardar(@ModelAttribute Usuario usuario){
+        usuarioService.guardar(usuario);
+        return "redirect:/vista/usuario";
     }
 }
