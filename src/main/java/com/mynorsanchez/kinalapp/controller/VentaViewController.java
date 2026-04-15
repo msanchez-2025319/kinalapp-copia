95684f688d13e9c4cab3d349d4aa4b644400b39e
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-04-15 02:48:17 -0600

    feat: se agrego en VentaViewController el endpoint de nuevo

--- src/main/java/com/mynorsanchez/kinalapp/controller/VentaViewController.java
+++ src/main/java/com/mynorsanchez/kinalapp/controller/VentaViewController.java
@@ -1,6 +1,7 @@
 package com.mynorsanchez.kinalapp.controller;
 
 
+import com.mynorsanchez.kinalapp.entity.Venta;
 import com.mynorsanchez.kinalapp.service.IVentaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,10 +15,15 @@ public class VentaViewController {
 
     @Autowired
     private IVentaService ventaService;
-    
+
     @GetMapping
     public String listar(Model model){
         model.addAttribute("venta", ventaService.listarTodo());
         return "venta/lista";
+    }
+    @GetMapping("/nuevo")
+    public String formularioNuevo(Model model){
+        model.addAttribute("venta", new Venta());
+        return "venta/formulario";
     }
 }
