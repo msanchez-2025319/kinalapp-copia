572b5845d77d578c6c85677aac02f7cb76317c04
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-04-15 02:03:00 -0600

    feat: se agrego en UsuarioViewController el endpoint de listar

--- src/main/java/com/mynorsanchez/kinalapp/controller/UsuarioViewController.java
+++ src/main/java/com/mynorsanchez/kinalapp/controller/UsuarioViewController.java
@@ -1,4 +1,21 @@
 package com.mynorsanchez.kinalapp.controller;
 
+import com.mynorsanchez.kinalapp.service.IUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vista/usuario")
 public class UsuarioViewController {
+    @Autowired
+    private IUsuarioService usuarioService;
+
+    @GetMapping
+    public String listar(Model model){
+        model.addAttribute("usuario", usuarioService.listarTodo());
+        return "usuario/lista";
+    }
 }
