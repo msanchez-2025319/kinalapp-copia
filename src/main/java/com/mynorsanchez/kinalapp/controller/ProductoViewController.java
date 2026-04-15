dd4fe77f26dffcd31223ebedeca8c1406335d2cd
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-04-15 01:17:17 -0600

    feat: se agrego el endpoint de guardar a ProductoViewController

--- src/main/java/com/mynorsanchez/kinalapp/controller/ProductoViewController.java
+++ src/main/java/com/mynorsanchez/kinalapp/controller/ProductoViewController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,5 +25,10 @@ public class ProductoViewController {
     public String formularioNuevo(Model model) {
         model.addAttribute("producto", new Producto());
         return "productos/formulario";
+     }
+     @GetMapping("/guardar")
+    public String guardar(@ModelAttribute Producto producto){
+        productoService.guardar(producto);
+        return "redirect:/vista/producto";
      }
 }
